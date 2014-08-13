4a71b147c38106cb911d0d89d848909f4ee83830
Albert Wohletz <albert@wohletz.net>
2014-08-12 20:34:43 -0700

    updated disertation. Made dynamic line widths so you can see orbit paths when zoomed out.

--- Unity/Assets/Scripts/MoveCamera.cs
+++ Unity/Assets/Scripts/MoveCamera.cs
@@ -8,14 +8,16 @@ public class MoveCamera : MonoBehaviour {
 	void Start () {
 		cam = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<Camera> ();
 	}
-	
+	const float trail_scale = -0.005f;
 	// Update is called once per frame
 	void Update () {
 		cam.transform.rotation = Quaternion.Euler(Vector3.zero);
 		if(Input.GetKeyDown ("a")){
 			cam.transform.Translate(0,0,-1 * cam.transform.localPosition.z/2);
+			UpdateTrailLengths(cam.transform.localPosition.z*trail_scale);
 		} else if (Input.GetKeyDown("o")){
 			cam.transform.Translate(0,0,cam.transform.localPosition.z/2);
+			UpdateTrailLengths(cam.transform.localPosition.z*trail_scale);
 		}
 		if (cam.transform.position.z >= 0){
 			//cam.transform.Translate (0,0,cam.transform.position.z * -2);
@@ -28,5 +30,13 @@ public class MoveCamera : MonoBehaviour {
 		cam.GetComponent<SmoothFollow> ().target = t.transform;
 		//cam.transform.parent = t.transform;
 //		cam.transform.localPosition = new Vector3 (0, 0, cam.transform.localPosition.z);
+	}
+
+	void UpdateTrailLengths(float value){
+		GameObject[] bodies = GameObject.FindGameObjectsWithTag ("Planet");
+		foreach (GameObject body in bodies){
+			body.GetComponent<TrailRenderer>().startWidth = value;
+			body.GetComponent<TrailRenderer>().endWidth = value;
+		}
 	}
 }
